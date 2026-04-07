cd9a77e127338dd6b2e21417b3cda0770f674a36
ひろし <neo862731@gmail.com>
2026-04-07 11:48:50 +0900

    Deploy: [UNIT-17] PWA_SYSTEM_INTEGRATION - Transform arcade into a standalone web app

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'lcp-arcade-v1';
+const ASSETS = [
+  'arcade.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
